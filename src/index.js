94615e8d9faabaea1e752fa4824bdfa25223182b
JuyLy <yuliya.zibarova@gmail.com>
2022-07-03 23:41:00 +0300

    Added unit converse

--- src/index.js
+++ src/index.js
@@ -15,15 +15,25 @@ let time = document.querySelector(".date");
 time.innerHTML = `${day} ${hours}:${minutes}`;
 
 function showTemperature(response) {
-  let city = response.data.name;
-  let temperature = Math.round(response.data.main.temp);
-  let weather = response.data.weather[0].main;
-  let currentCity = document.querySelector(".city");
-  currentCity.innerHTML = `${city}`;
-  let h1 = document.querySelector(".temp");
-  h1.innerHTML = `${temperature}`;
-  let li = document.querySelector(".weather");
-  li.innerHTML = `${weather}`;
+  let temperatureElement = document.querySelector("#temp");
+  let cityElement = document.querySelector(".city");
+  let descriptionElement = document.querySelector("#weather");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector(".date");
+  let iconElement = document.querySelector("#icon");
+  celsiusTemperature = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 function searchCity(city) {
   let apiKey = "e3a777fc1d55e0a271349e7b53f228c1";
@@ -36,6 +46,33 @@ function inputSubmit(event) {
   let city = `${search.value}`;
   searchCity(city);
 }
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temp");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheiTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheiTemperature);
+}
+
+function displayCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", inputSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
 searchCity("Kyiv");
